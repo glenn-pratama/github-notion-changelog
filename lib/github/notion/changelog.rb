e9bb3d03f2b99fe44551523dad331466e792e880
LPT-696\user <glenn.pratama@gmail.com>
2022-09-23 11:42:59 +0700

    Initial commit

---
+++ lib/github/notion/changelog.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "changelog/version"
+
+module Github
+  module Notion
+    module Changelog
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/github/notion/changelog_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Github::Notion::Changelog do
+  it "has a version number" do
+    expect(Github::Notion::Changelog::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
